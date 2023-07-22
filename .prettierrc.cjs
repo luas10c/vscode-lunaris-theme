4169f41ff3956429fdbf4c98125ed0a2c67684ec
Luciano Alves <luas10c@gmail.com>
2023-07-21 23:34:59 -0300

    chore: remove ignore eslint files

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,12 +1,14 @@
 /** @type{import('prettier').Config} */
 module.exports = {
-  "singleQuote": true,
-  "bracketSpacing": true,
-  "arrowParens": "always",
-  "bracketSameLine": true,
-  "trailingComma": "none",
-  "tabWidth": 2,
-  "quoteProps": "consistent",
-  "printWidth": 110,
-  "semi": false
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  bracketSameLine: true,
+  trailingComma: 'none',
+  tabWidth: 2,
+  quoteProps: 'consistent',
+  useTabs: false,
+  printWidth: 90,
+  semi: false,
+  plugins: []
 }
