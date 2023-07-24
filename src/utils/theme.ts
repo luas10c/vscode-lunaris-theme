ddf4ece81389bd3ad9be037fcace3bf96dba5c41
Luciano Alves <luas10c@gmail.com>
2023-07-24 10:40:46 -0300

    feat: applying colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,4 @@
-import { colors } from './colors.js'
-
-import type { Theme } from '#/types/theme'
+import { palette, colors } from './palette.js'
 
 export const theme = (name: string, bordered?: boolean) => {
   // vscode://schemas/color-theme
@@ -120,7 +118,7 @@ export const theme = (name: string, bordered?: boolean) => {
           'string punctuation.section.embedded source'
         ],
         settings: {
-          foreground: '#bbe488'
+          foreground: '#bfe6a3'
         }
       },
       {
@@ -374,28 +372,17 @@ export const theme = (name: string, bordered?: boolean) => {
     ],
     colors: {
       'foreground': '#cccccc',
-      'focusBorder': colors('cyan-500'),
-      'selection.background': colors('cyan-500', {
-        type: 'darken',
-        amount: 0.3
-      }),
+      'focusBorder': colors(palette.cyan).hex(),
+      'selection.background': colors(palette.cyan).darken(0.3).hex(),
       'scrollbar.shadow': '#000000',
-      'activityBar.foreground': colors('white', {
-        type: 'darken',
-        amount: 0.1
-      }),
-      'activityBar.background': colors('blue-500'),
-      'activityBar.inactiveForeground': colors('white', {
-        type: 'darken',
-        amount: 0.5
-      }),
-      'activityBar.activeBorder': colors('cyan-500'),
-      'activityBar.activeFocusBorder': colors('blue-500'),
+      'activityBar.foreground': colors(palette.white).darken(0.1).hex(),
+      'activityBar.background': colors(palette.stale).hex(),
+      'activityBar.inactiveForeground': colors(palette.white).darken(0.5).hex(),
+      'activityBar.activeBorder': colors(palette.cyan).hex(),
+      'activityBar.activeFocusBorder': colors(palette.stale).hex(),
       'activityBarBadge.foreground': '#ffffff',
-      'activityBarBadge.background': colors('cyan-500'),
-      'sideBar.background': colors('blue-500', {
-        amount: 0.02
-      }),
+      'activityBarBadge.background': colors(palette.cyan).hex(),
+      'sideBar.background': colors(palette.stale).lighten(0.02).hex(),
       'sideBar.foreground': '#cccccc',
       'sideBarSectionHeader.background': '#00000000',
       'sideBarSectionHeader.foreground': '#cccccc',
@@ -403,75 +390,57 @@ export const theme = (name: string, bordered?: boolean) => {
       'sideBarTitle.foreground': '#bbbbbb',
       'list.inactiveSelectionBackground': '#37373d',
       'list.inactiveSelectionForeground': '#cccccc',
-      'list.hoverBackground': colors('white', {
-        type: 'darken',
-        amount: 0.7
-      }),
+      'list.hoverBackground': colors(palette.white).darken(0.7).hex(),
       'list.hoverForeground': '#cccccc',
-      'list.activeSelectionBackground': colors('blue-500'),
+      'list.activeSelectionBackground': colors(palette.stale).hex(),
       'list.activeSelectionForeground': '#ffffff',
       'tree.indentGuidesStroke': '#585858',
       'list.dropBackground': '#383b3d',
-      'list.highlightForeground': colors('cyan-500'),
-      'list.focusBackground': colors('cyan-500', {
-        type: 'darken',
-        amount: 0.2
-      }),
+      'list.highlightForeground': colors(palette.cyan).hex(),
+      'list.focusBackground': colors(palette.cyan).darken(0.2).hex(),
       'list.focusForeground': '#cccccc',
       'listFilterWidget.background': '#653723',
       'listFilterWidget.outline': '#00000000',
       'listFilterWidget.noMatchesOutline': '#be1100',
       'statusBar.foreground': '#ffffff',
-      'statusBar.background': colors('blue-500'),
+      'statusBar.background': colors(palette.stale).hex(),
       'statusBarItem.hoverBackground': '#ffffff1f',
-      'statusBar.debuggingBackground': colors('orange-500'),
+      'statusBar.debuggingBackground': colors(palette.orange).hex(),
       'statusBar.debuggingForeground': '#ffffff',
-      'statusBar.noFolderBackground': colors('blue-500', {
-        amount: 0.1
-      }),
+      'statusBar.noFolderBackground': colors(palette.stale).lighten(0.1).hex(),
       'statusBar.noFolderForeground': '#ffffff',
-      'statusBarItem.remoteBackground': colors('cyan-500'),
+      'statusBarItem.remoteBackground': colors(palette.cyan).hex(),
       'statusBarItem.remoteForeground': '#ffffff',
-      'titleBar.activeBackground': colors('blue-500'),
+      'titleBar.activeBackground': colors(palette.stale).hex(),
       'titleBar.activeForeground': '#cccccc',
-      'titleBar.inactiveBackground': colors('blue-500', {
-        type: 'darken',
-        amount: 0.04
-      }),
+      'titleBar.inactiveBackground': colors(palette.stale).darken(0.04).hex(),
       'titleBar.inactiveForeground': '#cccccc99',
       'titleBar.border': '#00000000',
       'menubar.selectionForeground': '#cccccc',
       'menubar.selectionBackground': '#ffffff1a',
       'menu.foreground': '#cccccc',
-      'menu.background': colors('blue-500'),
+      'menu.background': colors(palette.stale).hex(),
       'menu.selectionForeground': '#ffffff',
-      'menu.selectionBackground': colors('cyan-500'),
+      'menu.selectionBackground': colors(palette.cyan).hex(),
       'menu.selectionBorder': '#00000000',
-      'menu.separatorBackground': colors('blue-500', {
-        amount: 0.05
-      }),
-      'menu.border': colors('blue-500', {
-        amount: 0.1
-      }),
-      'button.background': colors('cyan-500'),
+      'menu.separatorBackground': colors(palette.stale).lighten(0.05).hex(),
+      'menu.border': colors(palette.stale).lighten(0.1).hex(),
+      'button.background': colors(palette.cyan).hex(),
       'button.foreground': '#ffffff',
-      'button.hoverBackground': colors('cyan-500', {
-        amount: 0.1
-      }),
+      'button.hoverBackground': colors(palette.cyan).lighten(0.1).hex(),
       'button.secondaryForeground': '#ffffff',
       'button.secondaryBackground': '#3a3d41',
       'button.secondaryHoverBackground': '#45494e',
-      'input.background': colors('blue-500'),
+      'input.background': colors(palette.stale).hex(),
       'input.border': '#00000000',
       'input.foreground': '#cccccc',
-      'inputOption.activeBackground': colors('cyan-500', {
-        amount: 0.1
-      }),
+      'inputOption.activeBackground': colors(palette.cyan).lighten(0.1).hex(),
       'inputOption.activeBorder': '#007acc00',
       'inputOption.activeForeground': '#ffffff',
       'input.placeholderForeground': '#a6a6a6',
-      'textLink.foreground': colors('cyan-500'),
-      'editor.background': colors('blue-500'),
+      'textLink.foreground': colors(palette.cyan).hex(),
+      'textLink.activeForeground': colors(palette.cyan).lighten(0.1).hex(),
+      'editor.background': colors(palette.stale).hex(),
       'editor.foreground': '#d4d4d4',
       'editorLineNumber.foreground': '#858585',
       'editorCursor.foreground': '#aeafad',
@@ -495,7 +464,7 @@ export const theme = (name: string, bordered?: boolean) => {
       'editor.lineHighlightBackground': '#ffffff0A',
       'editor.lineHighlightBorder': '#282828',
       'editorLineNumber.activeForeground': '#c6c6c6',
-      'editorLink.activeForeground': '#4e94ce',
+      'editorLink.activeForeground': colors(palette.cyan).lighten(0.05).hex(),
       'editorIndentGuide.background': '#404040',
       'editorIndentGuide.activeBackground': '#707070',
       'editorRuler.foreground': '#5a5a5a',
@@ -513,7 +482,7 @@ export const theme = (name: string, bordered?: boolean) => {
       'editorInfo.foreground': '#75beff',
       'editorInfo.background': '#4490BF00',
       'editorInfo.border': '#4490BF00',
-      'editorGutter.background': colors('blue-500'),
+      'editorGutter.background': colors(palette.stale).hex(),
       'editorGutter.modifiedBackground': '#0c7d9d',
       'editorGutter.addedBackground': '#587c0c',
       'editorGutter.deletedBackground': '#94151b',
@@ -523,7 +492,7 @@ export const theme = (name: string, bordered?: boolean) => {
       'diffEditor.insertedTextBackground': '#54db681c',
       'diffEditor.removedTextBackground': '#6b29296c',
       'diffEditor.border': '#444444',
-      'panel.background': colors('blue-500'),
+      'panel.background': colors(palette.stale).hex(),
       'panel.border': '#80808059',
       'panelTitle.activeBorder': '#e7e7e7',
       'panelTitle.activeForeground': '#e7e7e7',
@@ -532,7 +501,7 @@ export const theme = (name: string, bordered?: boolean) => {
       'badge.foreground': '#ffffff',
       'terminal.foreground': '#cccccc',
       'terminal.selectionBackground': '#ffffff40',
-      'terminalCursor.background': colors('cyan-500'),
+      'terminalCursor.background': colors(palette.cyan).hex(),
       'terminalCursor.foreground': '#ffffff',
       'terminal.border': '#80808059',
       'terminal.ansiBlack': '#000000',
@@ -551,50 +520,39 @@ export const theme = (name: string, bordered?: boolean) => {
       'terminal.ansiRed': '#cd3131',
       'terminal.ansiWhite': '#e5e5e5',
       'terminal.ansiYellow': '#e5e510',
-      'breadcrumb.background': colors('blue-500'),
+      'breadcrumb.background': colors(palette.stale).hex(),
       'breadcrumb.foreground': '#cccccccc',
       'breadcrumb.focusForeground': '#e0e0e0',
-      'editorGroupHeader.tabsBackground': colors('blue-500'),
+      'editorGroupHeader.tabsBackground': colors(palette.stale).hex(),
       'tab.activeForeground': '#ffffff',
       'tab.border': '#252526',
-      'tab.activeBackground': colors('blue-500', {
-        type: 'darken',
-        amount: 0.05
-      }),
+      'tab.activeBackground': colors(palette.stale).darken(0.05).hex(),
       'tab.activeBorder': '#00000000',
-      'tab.activeBorderTop': colors('cyan-500'),
-      'tab.inactiveBackground': colors('blue-500'),
+      'tab.activeBorderTop': colors(palette.cyan).hex(),
+      'tab.inactiveBackground': colors(palette.stale).hex(),
       'tab.inactiveForeground': '#ffffff80',
       'scrollbarSlider.background': '#79797966',
       'scrollbarSlider.hoverBackground': '#646464b3',
       'scrollbarSlider.activeBackground': '#bfbfbf66',
-      'progressBar.background': colors('cyan-500'),
+      'progressBar.background': colors(palette.cyan).hex(),
       'widget.shadow': '#0000005c',
       'editorWidget.foreground': '#cccccc',
-      'editorWidget.background': colors('blue-500'),
+      'editorWidget.background': colors(palette.stale).hex(),
       'editorWidget.resizeBorder': '#5F5F5F',
       'pickerGroup.border': '#3f3f46',
-      'pickerGroup.foreground': colors('cyan-500', {
-        amount: 0.3
-      }),
+      'pickerGroup.foreground': colors(palette.cyan).lighten(0.3).hex(),
       'debugToolBar.background': '#333333',
       'debugToolBar.border': '#474747',
       'notifications.foreground': '#cccccc',
-      'notifications.background': colors('blue-500'),
+      'notifications.background': colors(palette.stale).hex(),
       'notificationToast.border': '#474747',
       'notificationsErrorIcon.foreground': '#f48771',
       'notificationsWarningIcon.foreground': '#cca700',
       'notificationsInfoIcon.foreground': '#75beff',
-      'notificationCenter.border': colors('blue-500', {
-        amount: 0.3
-      }),
+      'notificationCenter.border': colors(palette.stale).lighten(0.3).hex(),
       'notificationCenterHeader.foreground': '#cccccc',
-      'notificationCenterHeader.background': colors('blue-500', {
-        amount: 0.01
-      }),
-      'notifications.border': colors('blue-500', {
-        amount: 0.1
-      }),
+      'notificationCenterHeader.background': colors(palette.stale).lighten(0.01).hex(),
+      'notifications.border': colors(palette.stale).lighten(0.1).hex(),
       'gitDecoration.addedResourceForeground': '#81b88b',
       'gitDecoration.conflictingResourceForeground': '#6c6cc4',
       'gitDecoration.deletedResourceForeground': '#c74e39',
@@ -614,41 +572,26 @@ export const theme = (name: string, bordered?: boolean) => {
       'merge.incomingContentBackground': '#28384B',
       'merge.commonHeaderBackground': '#383838',
       'merge.commonContentBackground': '#282828',
-      'editorSuggestWidget.background': colors('blue-500'),
-      'editorSuggestWidget.border': colors('blue-500', {
-        amount: 0.1
-      }),
+      'editorSuggestWidget.background': colors(palette.stale).hex(),
+      'editorSuggestWidget.border': colors(palette.stale).lighten(0.1).hex(),
       'editorSuggestWidget.foreground': '#d4d4d4',
-      'editorSuggestWidget.highlightForeground': colors('cyan-500'),
-      'editorSuggestWidget.selectedBackground': colors('cyan-500', {
-        type: 'darken',
-        amount: 0.1
-      }),
+      'editorSuggestWidget.highlightForeground': colors(palette.cyan).hex(),
+      'editorSuggestWidget.selectedBackground': colors(palette.cyan).darken(0.1).hex(),
       'editorHoverWidget.foreground': '#cccccc',
-      'editorHoverWidget.background': colors('blue-500'),
-      'editorHoverWidget.border': colors('blue-500', {
-        amount: 0.1
-      }),
-      'peekView.border': colors('cyan-500'),
-      'peekViewEditor.background': colors('cyan-500', {
-        type: 'darken',
-        amount: 0.3
-      }),
-      'peekViewEditorGutter.background': colors('cyan-500', {
-        type: 'darken',
-        amount: 0.35
-      }),
+      'editorHoverWidget.background': colors(palette.stale).hex(),
+      'editorHoverWidget.border': colors(palette.stale).lighten(0.1).hex(),
+      'peekView.border': colors(palette.cyan).hex(),
+      'peekViewEditor.background': colors(palette.cyan).darken(0.3).hex(),
+      'peekViewEditorGutter.background': colors(palette.cyan).darken(0.35).hex(),
       'peekViewEditor.matchHighlightBackground': '#ff8f0099',
       'peekViewEditor.matchHighlightBorder': '#ee931e',
-      'peekViewResult.background': colors('blue-500'),
+      'peekViewResult.background': colors(palette.stale).hex(),
       'peekViewResult.fileForeground': '#ffffff',
       'peekViewResult.lineForeground': '#bbbbbb',
       'peekViewResult.matchHighlightBackground': '#ea5c004d',
       'peekViewResult.selectionBackground': '#3399ff33',
       'peekViewResult.selectionForeground': '#ffffff',
-      'peekViewTitle.background': colors('blue-500', {
-        amount: 0.06
-      }),
+      'peekViewTitle.background': colors(palette.stale).lighten(0.06).hex(),
       'peekViewTitleDescription.foreground': '#ccccccb3',
       'peekViewTitleLabel.foreground': '#ffffff',
       'icon.foreground': '#cccccc',
@@ -665,12 +608,9 @@ export const theme = (name: string, bordered?: boolean) => {
       'minimap.selectionHighlight': '#264f78',
       'minimap.errorHighlight': '#f48771',
       'minimap.warningHighlight': '#cca700',
-      'minimap.background': colors('blue-500', {
-        type: 'darken',
-        amount: 0.02
-      }),
+      'minimap.background': colors(palette.stale).darken(0.02).hex(),
       'sideBar.dropBackground': '#383b3d',
-      'editorGroup.emptyBackground': colors('blue-500'),
+      'editorGroup.emptyBackground': colors(palette.stale).hex(),
       'panelSection.border': '#80808059',
       'statusBarItem.activeBackground': '#FFFFFF25',
       'settings.headerForeground': '#cccccc',
