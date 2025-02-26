e42acdb53327e93639e094dd64f502838add213e
Luciano Alves <luas10c@gmail.com>
2025-02-26 11:25:00 -0300

    chore: update eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -2,35 +2,30 @@ import globals from 'globals'
 
 import js from '@eslint/js'
 
-import ts from '@typescript-eslint/eslint-plugin'
-import parser from '@typescript-eslint/parser'
+import ts from 'typescript-eslint'
 
 import prettier from 'eslint-plugin-prettier'
 
 /** @type{import('eslint').Linter.Config[]} */
-const config = [
+const config = ts.config(js.configs.recommended, ts.configs.recommended, [
   {
-    plugins: {
-      '@typescript-eslint': ts,
-      prettier
-    },
+    name: 'nodejs/recommended',
     languageOptions: {
-      parser,
-      parserOptions: {
-        ecmaVersion: 13,
-        sourceType: 'module'
-      },
       globals: {
         ...globals.es2022,
         ...globals.node
       }
+    }
+  },
+  {
+    name: 'prettier/recommended',
+    plugins: {
+      prettier
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...ts.configs.recommended.rules,
       ...prettier.configs.recommended.rules
     }
   }
-]
+])
 
 export default config
