54c6ca80f0c652e357632eb99cbfe3e05446872c
Luciano Alves <luas10c@gmail.com>
2025-09-01 14:37:09 -0300

    chore: add jest-plugin on eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,5 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+
 import globals from 'globals'
 
 import js from '@eslint/js'
@@ -6,26 +8,38 @@ import ts from 'typescript-eslint'
 
 import prettier from 'eslint-plugin-prettier'
 
-/** @type{import('eslint').Linter.Config[]} */
-const config = ts.config(js.configs.recommended, ts.configs.recommended, [
-  {
-    name: 'nodejs/recommended',
-    languageOptions: {
-      globals: {
-        ...globals.es2022,
-        ...globals.node
-      }
-    }
-  },
-  {
-    name: 'prettier/recommended',
-    plugins: {
-      prettier
-    },
-    rules: {
-      ...prettier.configs.recommended.rules
-    }
-  }
-])
+import jest from 'eslint-plugin-jest'
 
-export default config
+export default defineConfig([
+  globalIgnores(['node_modules', 'dist', 'coverage']),
+  ts.config(js.configs.recommended, ts.configs.recommended, [
+    {
+      name: 'nodejs/recommended',
+      languageOptions: {
+        globals: {
+          ...globals.es2022,
+          ...globals.node
+        }
+      }
+    },
+    {
+      name: 'prettier/recommended',
+      plugins: {
+        prettier
+      },
+      rules: {
+        ...prettier.configs.recommended.rules
+      }
+    },
+    {
+      name: 'jest/recommended',
+      files: ['**/*.{spec|test}.ts'],
+      languageOptions: {
+        globals: {
+          ...globals.jest
+        }
+      },
+      ...jest.configs['flat/recommended']
+    }
+  ])
+])
